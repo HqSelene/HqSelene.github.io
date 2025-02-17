74b107de5067a388f09c1c6f0b7d35358a482764
HqSelene <hqSelene@163.com>
2025-02-17 13:24:03 -0500

    modify tag-plugin

--- public/js/labels.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-hexo.extend.filter.register('before_post_render', function(data){
-  if (data.labels && !Array.isArray(data.labels)) {
-    data.labels = [data.labels];
-  }
-  return data;
-});
